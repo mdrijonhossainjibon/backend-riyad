bd9b233bc481a9356a1474d96cd62465914b4329
mdrijonhossainjibon <arntoken.tem@gmail.com>
2026-04-30 04:44:28 +0600

    first commit

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async (): Promise<void> => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/taskwave';
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://admin:Roman123@localhost:27017/taskwave';
     
     await mongoose.connect(mongoURI);
     
